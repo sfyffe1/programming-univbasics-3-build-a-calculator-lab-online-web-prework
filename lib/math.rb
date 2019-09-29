87f6096baf512192cb9aeafb0e7771c07e5fd066
Sean Fyffe <stfyffe@gmail.com>
2019-09-29 09:02:34 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,21 @@
 def addition(num1, num2)
-
+  num1 + num2 = sum
+  puts sum (4 , 5)
 end
 
 def subtraction(num1, num2)
-
+  num1 - num2 = dif
+  puts dif (10, 5)
 end
 
 def division(num1, num2)
-
+  num1 / num2 = div
+  puts div (50, 2)
 end
 
 def multiplication(num1, num2)
+  num1 * num2 = multiplication
+  puts multiplication(4, 30)
 
 end
 
